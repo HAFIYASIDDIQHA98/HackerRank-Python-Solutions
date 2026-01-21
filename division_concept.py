9ce8b9bb349f4c41d7b80fc008b9fa0a7710e848
Hafiya Siddiqha <hafiyasoftwaredeveloper1@gmail.com>
2026-01-21 21:30:24 +0530

    Added Integer and Float division logic
    Implemented floor division and float division as per HackerRank challenge.

---
+++ division_concept.py
@@ -0,0 +1,7 @@
+# Program to demonstrate Integer and Float Division
+a = int(input("Enter a Number :"))
+b = int(input("Enter b Number :"))
+
+print("Integer Division (Floor):", (a//b))
+print("Integer Division (Floor):", int(a/b))
+print("Float Division:", a/b)
