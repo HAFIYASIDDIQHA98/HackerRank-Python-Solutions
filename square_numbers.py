40415b553c57805e2c3e86cdd1b4c9924502a5de
Hafiya Siddiqha <hafiyasoftwaredeveloper1@gmail.com>
2026-01-21 21:38:26 +0530

    Added squares of numbers using for loop
    This program takes an integer n and prints the square of each number from 0 to n-1.

---
+++ square_numbers.py
@@ -0,0 +1,6 @@
+# Program to print squares of numbers up to n
+n = int(input("Enter the limit: "))
+
+for i in range(n):
+    # Printing the square of each number
+    print(f"Square of {i} is:", i**2)
