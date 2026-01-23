014f66abec56b1da65e6b21971d210eab18d69b9
Hafiya Siddiqha <hafiyasoftwaredeveloper1@gmail.com>
2026-01-23 19:13:28 +0530

    apply set operations for unique data filtering
    This file demonstrates the use of Python Sets to handle unique data. I practiced set initialization and mathematical operations like intersection to find common elements between two datasets, which is a common requirement in data processing.

---
+++ student_analyzer.py
@@ -0,0 +1,9 @@
+# Topic: Sets and Unique Data
+guest_list = {"Ali", "Sara", "Hafiya", "Ali"} # Duplicate 'Ali' removed
+print("Unique Guests:", guest_list)
+
+# Skills Example
+python_devs = {"Hafiya", "Sara"}
+java_devs = {"Hafiya", "Ali"}
+
+print("Both Skills:", python_devs.intersection(java_devs))
